6aac761f0b67d193ca1bcadc3b157272afb81965
Administrator <Administrator@192.168.0.105>
2018-03-29 22:09:44 +0800

    添加桥接模式

---
+++ myproject-designmodel/src/main/java/builder/ReadMe.txt
@@ -0,0 +1,11 @@
+建造者模式
+优点：
+
+缺点：
+
+
+
+
+
+建造者模式与工厂模式区别
+http://hejiawangjava.iteye.com/blog/2238889
---
+++ myproject-designmodel/src/main/java/prototype/PrototypeDemo.java
@@ -0,0 +1,30 @@
+package prototype;
+
+class Prototype implements Cloneable {
+	public Prototype clone() {
+		Prototype prototype = null;
+		try {
+			prototype = (Prototype) super.clone();
+		} catch (CloneNotSupportedException e) {
+			e.printStackTrace();
+		}
+		return prototype;
+	}
+}
+
+class ConcretePrototype extends Prototype {
+	public void show() {
+		System.out.println("原型模式实现类");
+	}
+}
+
+public class PrototypeDemo {
+	public static void main(String[] args) {
+		ConcretePrototype cp = new ConcretePrototype();
+		for (int i = 0; i < 10; i++) {
+			ConcretePrototype clonecp = (ConcretePrototype) cp.clone();
+			clonecp.show();
+		}
+	}
+
+}
