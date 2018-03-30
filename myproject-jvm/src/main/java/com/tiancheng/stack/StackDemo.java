291934c7991dc30c2092929d73881c0c312d399b
song218888 <522658066@qq.com>
2018-03-30 17:24:04 +0800

    虚拟机栈深度测试

---
+++ myproject-jvm/src/main/java/com/tiancheng/stack/StackDemo.java
@@ -0,0 +1,28 @@
+package com.tiancheng.stack;
+
+/**
+ * 
+ * @author DELL
+ * 
+ */
+public class StackDemo {
+	private int count = 0;
+
+	public void testAdd() {
+		count++;
+		testAdd();
+	}
+
+	public void test() {
+		try {
+			testAdd();
+		} catch (Throwable e) {
+			System.out.println(e);
+			System.out.println("栈深度:" + count);
+		}
+	}
+
+	public static void main(String[] args) {
+		new StackDemo().test();
+	}
+}
