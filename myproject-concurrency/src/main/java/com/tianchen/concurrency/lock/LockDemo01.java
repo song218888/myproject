5f6b1d9f1bb7067057f125c5793ecd66bd8ffd04
dell <dell@192.168.0.105>
2018-04-24 21:57:48 +0800

    添加lock的demo

--- myproject-concurrency/src/main/java/com/tianchen/concurrency/lock/LockDemo01.java
+++ myproject-concurrency/src/main/java/com/tianchen/concurrency/lock/LockDemo01.java
@@ -1,8 +1,20 @@
 package com.tianchen.concurrency.lock;
 
+import java.util.concurrent.TimeUnit;
 import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
 
 public class LockDemo01 {
 	public static void main(String[] args) {
+		
+		Lock lock = new ReentrantLock();
+		
+		try {
+			lock.tryLock(500, TimeUnit.MICROSECONDS);
+		} catch (Exception e) {
+			// TODO: handle exception
+		}finally{
+			lock.unlock();
+		}
 	}
 }
---
+++ myproject-concurrency/src/main/java/com/tianchen/concurrency/lock/ReetranLockDemo01.java
@@ -0,0 +1,41 @@
+package com.tianchen.concurrency.lock;
+
+import java.util.ArrayList;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
+public class ReetranLockDemo01 {
+	private ArrayList<Integer> arrayList = new ArrayList<Integer>();
+
+	public static void main(String[] args) {
+		final ReetranLockDemo01 reetranLockDemo01 = new ReetranLockDemo01();
+
+		new Thread() {
+			public void run() {
+				reetranLockDemo01.insert(Thread.currentThread());
+			};
+		}.start();
+		
+		new Thread() {
+			public void run() {
+				reetranLockDemo01.insert(Thread.currentThread());
+			};
+		}.start();
+	}
+
+	public void insert(Thread thread) {
+		Lock lock = new ReentrantLock();
+		lock.lock();
+		try {
+			System.out.println(thread.getName() + " 得到了锁");
+			for (int i = 0; i < 5; i++) {
+				arrayList.add(i);
+			}
+		} catch (Exception e) {
+		} finally {
+			System.out.println(thread.getName()+" 释放了锁");
+			lock.unlock();
+		}
+	}
+
+}
