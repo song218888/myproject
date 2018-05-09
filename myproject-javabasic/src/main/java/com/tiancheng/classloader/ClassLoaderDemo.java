21b6c54d05f2c0fe3b1197143aea20b137505be9
songhuan <songhuan@niiwoo.com>
2018-05-09 11:36:58 +0800

    更新目录，将javabasic下的jvm与classloader移至jvm目录下

--- myproject-javabasic/src/main/java/com/tiancheng/classloader/ClassLoaderDemo.java
+++
@@ -1,7 +0,0 @@
-package com.tiancheng.classloader;
-
-public class ClassLoaderDemo {
-	public static void main(String[] args) {
-		
-	}
-}
---
+++ myproject-jvm/src/main/java/com/tiancheng/classloader/ClassLoaderDemo.java
@@ -0,0 +1,42 @@
+package com.tiancheng.classloader;
+
+class Singleton {
+    private static int counter1;  
+    private static int counter2 = 0;  
+    private static Singleton singleton=new Singleton();  
+    
+    public Singleton() {  
+       counter1++;  
+       counter2++;  
+    }  
+    
+    public static int getCounter1() {  
+       return counter1;  
+    } 
+    
+    public static int getCounter2() {  
+       return counter2;  
+    }  
+    /** 
+     * @Description:实例化 
+     * @return 
+     * @author YHJ create at 2011-6-4 下午08:34:43 
+     */  
+    public static Singleton getInstance(){  
+       return singleton;  
+    }  
+} 
+
+public class ClassLoaderDemo {  
+    /** 
+     * @Description:启动类 
+     * @param args 
+     * @author YHJ create at 2011-6-4 下午08:30:12 
+     */  
+    @SuppressWarnings("static-access")  
+    public static void main(String[] args) {  
+       Singleton singleton=Singleton.getInstance();  
+       System.out.println("counter1:"+singleton.getCounter1());  
+       System.out.println("counter2:"+singleton.getCounter2());  
+    }  
+}
