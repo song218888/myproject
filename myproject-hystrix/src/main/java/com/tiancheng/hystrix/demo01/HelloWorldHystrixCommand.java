a455be30b0553e08ffea2a367059c0145d915911
dell <dell@DESKTOP-BHR3CBO.mshome.net>
2018-05-26 21:08:39 +0800

    修改文件

--- myproject-hystrix/src/main/java/com/tiancheng/hystrix/demo01/HelloWorldHystrixCommand.java
+++ myproject-hystrix/src/main/java/com/tiancheng/hystrix/demo01/HelloWorldHystrixCommand.java
@@ -1,47 +1,49 @@
 package com.tiancheng.hystrix.demo01;
 
-import java.util.concurrent.TimeUnit;
-
 import com.netflix.hystrix.HystrixCommand;
 import com.netflix.hystrix.HystrixCommandGroupKey;
-import com.netflix.hystrix.HystrixCommandKey;
-import com.netflix.hystrix.HystrixCommandProperties;
-import com.netflix.hystrix.HystrixThreadPoolKey;
 
 public class HelloWorldHystrixCommand extends HystrixCommand<String> {
 
-	private final String name;
+	private String name;
 
 	public HelloWorldHystrixCommand(String name) {
-		// TODO Auto-generated constructor stub
-		// super(HystrixCommandGroupKey.Factory.asKey("ExampleGroup"));
-		super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("testCommandGroupKey"))
-				.andCommandKey(HystrixCommandKey.Factory.asKey("testCommandkey"))
-				.andThreadPoolKey(HystrixThreadPoolKey.Factory.asKey("testThreadPool"))
-				.andCommandPropertiesDefaults(HystrixCommandProperties.Setter()
-						// .withExecutionIsolationStrategy(ExecutionIsolationStrategy.SEMAPHORE)
-						.withExecutionTimeoutInMilliseconds(5000)));
+		// 最少配置:指定命令组名(CommandGroup)
+		 super(HystrixCommandGroupKey.Factory.asKey("aa"));
 
-		// HystrixCommandProperties.Setter().withCircuitBreakerEnabled(true);
-		// HystrixCollapserProperties.Setter()
-		// HystrixThreadPoolProperties.Setter().withCoreSize(1);
+//		super(Setter.withGroupKey(HystrixCommandGroupKey.Factory.asKey("testCommandGroupKey"))
+//				.andCommandKey(HystrixCommandKey.Factory.asKey("testCommandKey"))
+//				.andThreadPoolKey(HystrixThreadPoolKey.Factory.asKey("testThreadPool"))
+//				.andCommandPropertiesDefaults(
+//						HystrixCommandProperties.Setter()
+//								.withExecutionIsolationStrategy(ExecutionIsolationStrategy.SEMAPHORE)
+//								.withExecutionTimeoutInMilliseconds(5000)
+//								.withCircuitBreakerEnabled(true)
+//						)
+//				);
+		
+//		HystrixCommandProperties.Setter().withCircuitBreakerEnabled(true);
+//		HystrixCommandProperties.Setter().withExecutionIsolationStrategy(ExecutionIsolationStrategy.SEMAPHORE);
+//		HystrixCommandProperties.Setter().withExecutionTimeoutInMilliseconds(5000);
+//		HystrixThreadPoolProperties.Setter().withCoreSize(1);
+		
 		this.name = name;
+
 	}
 	
-//	@Override  
-//  protected String getFallback() {
+//	@Override
+//	protected String getFallback() {
 //		System.out.println("触发了降级!");
 //      return "exeucute fallback";
-//  }
-	
+//	}
 
 	@Override
-	protected String run() throws Exception {
-		 for (int i = 0; i < 10; i++) {
-		 System.out.println("runing HelloWorldHystrixCommand..." + i);
-		 }
+	protected String run(){
+		for (int i = 0; i < 10; i++) {
+			System.out.println("runing HelloWorldHystrixCommand..." + i);
+		}
+		return "【Hello " + name + "! thread:" + Thread.currentThread().getName()+"】";
 		
-		 TimeUnit.MILLISECONDS.sleep(3000);
-		return "Hello " + name + "! thread:" + Thread.currentThread().getName();
 	}
+
 }
