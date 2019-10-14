4ffbe974c96e8f2cba401e1450f11bbd1073c3f1
songhuan01 <songhuan01@ky-tech.com.cn>
2019-10-14 15:02:33 +0800

    提交POM文件修改 去掉hbase demo类

--- myproject-hbase/src/main/java/com/tianchen/util/UsersTool.java
+++ myproject-hbase/src/main/java/com/tianchen/util/UsersTool.java
@@ -1,51 +1,41 @@
 package com.tianchen.util;
 
 import java.io.IOException;
-import java.util.List;
-
-import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.hbase.HBaseConfiguration;
-import org.apache.hadoop.hbase.client.HBaseAdmin;
-import org.apache.hadoop.hbase.client.HTablePool;
-import org.apache.log4j.Logger;
-
-import com.tianchen.dao.UsersDAO;
-import com.tianchen.model.User;
 
 public class UsersTool {
 
-	private static final Logger log = Logger.getLogger(UsersTool.class);
-	
-	private static final String QUOREM = "192.168.18.76,192.168.18.82,192.168.18.83";
-	private static final String CLIENT_PORT = "2181";
-	private HBaseAdmin admin;
-	private Configuration conf;
-	
-	
-	public HBaseAdmin getHBaseAdmin(){
-		getConfiguration();
-        try {
-			admin = new HBaseAdmin(conf);
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-		return admin; 
-	}
-	
-	public Configuration getConfiguration(){
-		if(conf == null){
-			conf = HBaseConfiguration.create();
-	        conf.set("hbase.zookeeper.quorum", QUOREM);   
-	        conf.set("hbase.zookeeper.property.clientPort", CLIENT_PORT);   
-		}
-		return conf;
-	}
-	
+//	private static final Logger log = Logger.getLogger(UsersTool.class);
+//	
+//	private static final String QUOREM = "192.168.18.76,192.168.18.82,192.168.18.83";
+//	private static final String CLIENT_PORT = "2181";
+//	private HBaseAdmin admin;
+//	private Configuration conf;
+//	
+//	
+//	public HBaseAdmin getHBaseAdmin(){
+//		getConfiguration();
+//        try {
+//			admin = new HBaseAdmin(conf);
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		return admin; 
+//	}
+//	
+//	public Configuration getConfiguration(){
+//		if(conf == null){
+//			conf = HBaseConfiguration.create();
+//	        conf.set("hbase.zookeeper.quorum", QUOREM);   
+//	        conf.set("hbase.zookeeper.property.clientPort", CLIENT_PORT);   
+//		}
+//		return conf;
+//	}
+//	
 
 	public static void main(String[] args) throws IOException {
 		
-		UsersTool tools = new UsersTool();
-		tools.getConfiguration();
+//		UsersTool tools = new UsersTool();
+//		tools.getConfiguration();
 		
 
 //		HTablePool pool = new HTablePool();
